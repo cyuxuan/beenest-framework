0fd6af3f7a3a65c7e2b0eaeb72dc39c01b21ae7d
cyuxuan <cyuxuan@163.com>
2025-11-01 02:27:53 +0800

    feat(初始化): 项目初始化

---
+++ system-common/src/main/java/club/beenest/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package club.beenest.common.exception.user;
+
+import club.beenest.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
